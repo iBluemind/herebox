a10bcb77b641218a57d8076d100f830ed0828512
Han Manjong <han@manjong.org>
2016-04-27 04:01:30 +0900

    픽업 요청 구현 중

--- hereboxweb/admin/__init__.py
+++ hereboxweb/admin/__init__.py
@@ -2,4 +2,4 @@
 
 from flask import Blueprint
 
-admin = Blueprint('admin', __name__, template_folder='templates')
+admin = Blueprint('admin', __name__, template_folder='templates', url_prefix='/admin')
