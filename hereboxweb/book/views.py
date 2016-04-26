b2b1d01579a6ba4d6228d681374937dad8c2b406
Han Manjong <han@manjong.org>
2016-04-27 03:37:43 +0900

    자동 결제 신청 오류 수정 및 창고 관리 API 추가

--- hereboxweb/book/views.py
+++ hereboxweb/book/views.py
@@ -9,7 +9,8 @@ from flask.ext.login import login_required, current_user
 from hereboxweb import database, response_template
 from hereboxweb.book.models import *
 from hereboxweb.book import book
-from hereboxweb.schedule.models import NewReservation, DeliveryReservation, RestoreReservation, Reservation
+from hereboxweb.schedule.models import NewReservation, DeliveryReservation, RestoreReservation, Reservation, Schedule, \
+    ScheduleType, ReservationStatus, ScheduleStatus
 from hereboxweb.utils import staff_required, add_months
 
 STUFF_LIST_MAX_COUNT = 10
@@ -44,9 +45,26 @@ def my_stuff():
                            packed_my_stuffs=packed_my_stuffs)
 
 
-@book.route('/goods', methods=['POST'])
+@book.route('/reservation/accept', methods=['POST'])
 # @staff_required
-def goods():
+def accept_reservation():
+    reservation_id = request.form.get('reservation_id')
+    reservation = Reservation.query.filter(Reservation.reservation_id == reservation_id).first()
+    if not reservation:
+        return response_template(u'%s 주문을 찾을 수 없습니다.' % reservation_id, status=400)
+
+    reservation.status = ReservationStatus.ACCEPTED
+
+    try:
+        database.session.commit()
+    except:
+        return response_template(u'오류가 발생했습니다.', status=500)
+    return response_template(u'정상 처리되었습니다.')
+
+
+@book.route('/goods/allocate', methods=['POST'])
+# @staff_required
+def allocate_goods():
     goods_type = request.form.get('goods_type')
     reservation_id = request.form.get('reservation_id')
     name = request.form.get('name')
@@ -60,9 +78,19 @@ def goods():
         if not box:
             return response_template(u'%s 상자를 찾을 수 없습니다.' % box_id, status=400)
 
-    reservation = Reservation.query.filter(Reservation.reservation_id == reservation_id).first()
+    reservation = Reservation.query.filter(
+                        Reservation.reservation_id == reservation_id,
+                        Reservation.status == ReservationStatus.ACCEPTED).first()
     if not reservation:
-        return response_template(u'%s 주문을 찾을 수 없습니다.' % reservation_id, status=400)
+        return response_template(u'접수된 주문 %s을 찾을 수 없습니다.' % reservation_id, status=400)
+
+    schedules = reservation.schedules
+    pickup_schedules = []
+    for schedule in schedules:
+        if schedule.schedule_type == ScheduleType.PICKUP_DELIVERY or\
+                schedule.schedule_type == ScheduleType.PICKUP_RECOVERY:
+            schedule.status = ScheduleStatus.COMPLETE
+            pickup_schedules.append(schedule)
 
     today = datetime.date.today()
     expired = add_months(today, reservation.period)
@@ -73,11 +101,48 @@ def goods():
                       in_store=InStoreStatus.IN_STORE,
                       box_id=box.id if box else None,
                       user_id=user_id,
-                      reservations=[Reservation.query.get(reservation.id)],
-                      expired_at=expired)
+                      schedules=pickup_schedules,
+                      expired_at=expired,
+                      fixed_rate=reservation.fixed_rate)
 
     try:
         database.session.add(new_goods)
+        database.session.commit()
+    except:
+        return response_template(u'오류가 발생했습니다.', status=500)
+    return response_template(u'정상 처리되었습니다.')
+
+
+@book.route('/goods/store', methods=['POST'])
+# @staff_required
+def store_goods():
+    goods_id = request.form.get('goods_id')
+
+    goods = Goods.query.filter(Goods.goods_id == goods_id,
+                                Goods.in_store == InStoreStatus.OUT_OF_STORE)\
+                            .first()
+
+    goods.in_store = InStoreStatus.IN_STORE
+
+    try:
+        database.session.commit()
+    except:
+        return response_template(u'오류가 발생했습니다.', status=500)
+    return response_template(u'정상 처리되었습니다.')
+
+
+@book.route('/goods/release', methods=['POST'])
+# @staff_required
+def release_goods():
+    goods_id = request.form.get('goods_id')
+
+    goods = Goods.query.filter(Goods.goods_id == goods_id,
+                                Goods.in_store == InStoreStatus.IN_STORE)\
+                            .first()
+
+    goods.in_store = InStoreStatus.OUT_OF_STORE
+
+    try:
         database.session.commit()
     except:
         return response_template(u'오류가 발생했습니다.', status=500)
