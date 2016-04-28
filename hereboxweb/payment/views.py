9fc6cd87b3aa0cce67697986cca9c44afd1c7c28
Han Manjong <han@manjong.org>
2016-04-28 12:56:24 +0900

    문자 알림 수정

--- hereboxweb/payment/views.py
+++ hereboxweb/payment/views.py
@@ -584,8 +584,8 @@ def reservation_payment():
             return response_template(u'문제가 발생했습니다. 나중에 다시 시도해주세요.', 500)
 
         visit_time = VisitTime.query.get(visit_time)
-        pay_type = '현장' if new_reservation.pay_type == PayType.DIRECT else '온라인'
-        send_mms.apply_async(args=['01064849686', u'[히어박스] 주문번호: %s / 이름: %s / 예약시간: %s %s / 유형: 신규 / 결제방법: %s'
+        pay_type = u'현장' if new_reservation.pay_type == PayType.DIRECT else u'온라인'
+        send_mms.apply_async(args=['01088018572', u'[히어박스] 주문번호: %s / 이름: %s / 예약시간: %s %s / 유형: 신규 / 결제방법: %s'
                                                   u' / 주소: %s / 연락처: %s / 규격(갯수): %s / 비규격(갯수): %s'
                                                   u' / 보관기간: %s개월 / 남길말: %s ' % (
                                        new_visit_schedule.schedule_id, current_user.name, visit_date, visit_time,
@@ -596,14 +596,14 @@ def reservation_payment():
 
         if new_revisit_schedule:
             revisit_time = VisitTime.query.get(revisit_time)
-            send_mms.apply_async(args=['01064849686', u'[히어박스] 주문번호: %s / 이름: %s / 예약시간: %s %s / 유형: 신규 / 결제방법: %s'
+            send_mms.apply_async(args=['01088018572', u'[히어박스] 주문번호: %s / 이름: %s / 예약시간: %s %s / 유형: 신규 / 결제방법: %s'
                                                       u' / 주소: %s / 연락처: %s / 규격(갯수): %s / 비규격(갯수): %s'
                                                       u' / 보관기간: %s개월 / 남길말: %s ' % (
                                            new_revisit_schedule.schedule_id, current_user.name, revisit_date, revisit_time,
-                                           pay_type, new_revisit_schedule.address, current_user.phone,
-                                           new_revisit_schedule.standard_box_count,
-                                           new_revisit_schedule.nonstandard_goods_count, new_revisit_schedule.period,
-                                           new_revisit_schedule.user_memo)])
+                                           pay_type, new_reservation.address, current_user.phone,
+                                           new_reservation.standard_box_count,
+                                           new_reservation.nonstandard_goods_count, new_reservation.period,
+                                           new_reservation.user_memo)])
         return response_template(u'정상 처리되었습니다', 200)
 
     estimate_info = get_estimate()
