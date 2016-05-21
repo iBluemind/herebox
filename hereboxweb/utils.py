a6ba5225609618c9bc008de6e8f9dc169f1dae0f
Han Manjong <han@manjong.org>
2016-05-22 06:31:58 +0900

    Flask-Compressor 적용

--- hereboxweb/utils.py
+++ hereboxweb/utils.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import re
+
+from flask.ext.compressor import FileAsset, CSSBundle, JSBundle
 from sqlalchemy.sql import ClauseElement
 from wtforms.validators import Required
 
@@ -123,3 +125,243 @@ class RequiredIf(Required):
             raise Exception('no field named "%s" in form' % self.other_field_name)
         if bool(other_field.data):
             super(RequiredIf, self).__call__(form, field)
+
+
+def compress():
+    from hereboxweb import compressor
+
+    herebox_css = FileAsset(filename='assets/css/herebox.css', processors=['cssmin'])
+    herebox_css_bundle = CSSBundle(
+        name='herebox_css_bundle',
+        assets=[
+            herebox_css,
+        ],
+    )
+    compressor.register_bundle(herebox_css_bundle)
+
+    herebox_mobile_css = FileAsset(filename='assets/css/herebox_mobile.css', processors=['cssmin'])
+    herebox_mobile_css_bundle = CSSBundle(
+        name='herebox_mobile_css_bundle',
+        assets=[
+            herebox_mobile_css,
+        ],
+    )
+    compressor.register_bundle(herebox_mobile_css_bundle)
+
+    completion_js = FileAsset(filename='assets/js/completion.js', processors=['jsmin'])
+    completion_js_bundle = JSBundle(
+        name='completion_js_bundle',
+        assets=[
+            completion_js,
+        ],
+    )
+    compressor.register_bundle(completion_js_bundle)
+
+    delivery_payment_js = FileAsset(filename='assets/js/delivery_payment.js', processors=['jsmin'])
+    delivery_payment_js_bundle = JSBundle(
+        name='delivery_payment_js_bundle',
+        assets=[
+            delivery_payment_js,
+        ],
+    )
+    compressor.register_bundle(delivery_payment_js_bundle)
+
+    delivery_reservation_js = FileAsset(filename='assets/js/delivery_reservation.js', processors=['jsmin'])
+    delivery_reservation_js_bundle = JSBundle(
+        name='delivery_reservation_js_bundle',
+        assets=[
+            delivery_reservation_js,
+        ],
+    )
+    compressor.register_bundle(delivery_reservation_js_bundle)
+
+    delivery_review_js = FileAsset(filename='assets/js/delivery_review.js', processors=['jsmin'])
+    delivery_review_js_bundle = JSBundle(
+        name='delivery_review_js',
+        assets=[
+            delivery_review_js,
+        ],
+    )
+    compressor.register_bundle(delivery_review_js_bundle)
+
+    estimate_js = FileAsset(filename='assets/js/estimate.js', processors=['jsmin'])
+    estimate_js_bundle = JSBundle(
+        name='estimate_js_bundle',
+        assets=[
+            estimate_js,
+        ],
+    )
+    compressor.register_bundle(estimate_js_bundle)
+
+    extended_completion_js = FileAsset(filename='assets/js/extended_completion.js', processors=['jsmin'])
+    extended_completion_js_bundle = JSBundle(
+        name='extended_completion_js_bundle',
+        assets=[
+            extended_completion_js,
+        ],
+    )
+    compressor.register_bundle(extended_completion_js_bundle)
+
+    extended_estimate_js = FileAsset(filename='assets/js/extended_estimate.js', processors=['jsmin'])
+    extended_estimate_js_bundle = JSBundle(
+        name='extended_estimate_js_bundle',
+        assets=[
+            extended_estimate_js,
+        ],
+    )
+    compressor.register_bundle(extended_estimate_js_bundle)
+
+    extended_payment_js = FileAsset(filename='assets/js/extended_payment.js', processors=['jsmin'])
+    extended_payment_js_bundle = JSBundle(
+        name='extended_payment_js_bundle',
+        assets=[
+            extended_payment_js,
+        ],
+    )
+    compressor.register_bundle(extended_payment_js_bundle)
+
+    extended_review_js = FileAsset(filename='assets/js/extended_review.js', processors=['jsmin'])
+    extended_review_js_bundle = JSBundle(
+        name='extended_review_js_bundle',
+        assets=[
+            extended_review_js,
+        ],
+    )
+    compressor.register_bundle(extended_review_js_bundle)
+
+    hbcount_js = FileAsset(filename='assets/js/hbcount.js', processors=['jsmin'])
+    hbcount_js_bundle = JSBundle(
+        name='hbcount_js_bundle',
+        assets=[
+            hbcount_js,
+        ],
+    )
+    compressor.register_bundle(hbcount_js_bundle)
+
+    herebox_js = FileAsset(filename='assets/js/herebox.js', processors=['jsmin'])
+    herebox_js_bundle = JSBundle(
+        name='herebox_js_bundle',
+        assets=[
+            herebox_js,
+        ],
+    )
+    compressor.register_bundle(herebox_js_bundle)
+
+    introduce_js = FileAsset(filename='assets/js/introduce.js', processors=['jsmin'])
+    introduce_js_bundle = JSBundle(
+        name='introduce_js_bundle',
+        assets=[
+            introduce_js,
+        ],
+    )
+    compressor.register_bundle(introduce_js_bundle)
+
+    login_js = FileAsset(filename='assets/js/login.js', processors=['jsmin'])
+    login_js_bundle = JSBundle(
+        name='login_js_bundle',
+        assets=[
+            login_js,
+        ],
+    )
+    compressor.register_bundle(login_js_bundle)
+
+    my_info_js = FileAsset(filename='assets/js/my_info.js', processors=['jsmin'])
+    my_info_js_bundle = JSBundle(
+        name='my_info_js_bundle',
+        assets=[
+            my_info_js,
+        ],
+    )
+    compressor.register_bundle(my_info_js_bundle)
+
+    my_schedule_js = FileAsset(filename='assets/js/my_schedule.js', processors=['jsmin'])
+    my_schedule_js_bundle = JSBundle(
+        name='my_schedule_js_bundle',
+        assets=[
+            my_schedule_js,
+        ],
+    )
+    compressor.register_bundle(my_schedule_js_bundle)
+
+    my_stuff_js = FileAsset(filename='assets/js/my_stuff.js', processors=['jsmin'])
+    my_stuff_js_bundle = JSBundle(
+        name='my_stuff_js_bundle',
+        assets=[
+            my_stuff_js,
+        ],
+    )
+    compressor.register_bundle(my_stuff_js_bundle)
+
+    pickup_payment_js = FileAsset(filename='assets/js/pickup_payment.js', processors=['jsmin'])
+    pickup_payment_js_bundle = JSBundle(
+        name='pickup_payment_js_bundle',
+        assets=[
+            pickup_payment_js,
+        ],
+    )
+    compressor.register_bundle(pickup_payment_js_bundle)
+
+    pickup_reservation_js = FileAsset(filename='assets/js/pickup_reservation.js', processors=['jsmin'])
+    pickup_reservation_js_bundle = JSBundle(
+        name='pickup_reservation_js_bundle',
+        assets=[
+            pickup_reservation_js,
+        ],
+    )
+    compressor.register_bundle(pickup_reservation_js_bundle)
+
+    pickup_review_js = FileAsset(filename='assets/js/pickup_review.js', processors=['jsmin'])
+    pickup_review_js_bundle = JSBundle(
+        name='pickup_review_js_bundle',
+        assets=[
+            pickup_review_js,
+        ],
+    )
+    compressor.register_bundle(pickup_review_js_bundle)
+
+    reservation_js = FileAsset(filename='assets/js/reservation.js', processors=['jsmin'])
+    reservation_js_bundle = JSBundle(
+        name='reservation_js_bundle',
+        assets=[
+            reservation_js,
+        ],
+    )
+    compressor.register_bundle(reservation_js_bundle)
+
+    reservation_payment_js  = FileAsset(filename='assets/js/reservation_payment.js', processors=['jsmin'])
+    reservation_payment_js_bundle = JSBundle(
+        name='reservation_payment_js_bundle',
+        assets=[
+            reservation_payment_js,
+        ],
+    )
+    compressor.register_bundle(reservation_payment_js_bundle)
+
+    review_js = FileAsset(filename='assets/js/review.js', processors=['jsmin'])
+    review_js_bundle = JSBundle(
+        name='review_js_bundle',
+        assets=[
+            review_js,
+        ],
+    )
+    compressor.register_bundle(review_js_bundle)
+
+    signup_js = FileAsset(filename='assets/js/signup.js', processors=['jsmin'])
+    signup_js_bundle = JSBundle(
+        name='signup_js_bundle',
+        assets=[
+            signup_js,
+        ],
+    )
+    compressor.register_bundle(signup_js_bundle)
+
+    my_schedule_mobile_js = FileAsset(filename='assets/js/mobile/my_schedule.js', processors=['jsmin'])
+    my_schedule_mobile_js_bundle = JSBundle(
+        name='my_schedule_mobile_js_bundle',
+        assets=[
+            my_schedule_mobile_js,
+        ],
+    )
+    compressor.register_bundle(my_schedule_mobile_js_bundle)
+
+
